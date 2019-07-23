8730f46531dd4a31240a790fba4789aee0865d0e
Robin de Mourat <robin.demourat@gmail.com>
2019-07-23 11:31:05 +0200

    improve archive builder

--- api/scripts/utils/build-archive-index.js
+++ api/scripts/utils/build-archive-index.js
@@ -38,7 +38,7 @@ const buildInstanceArchive = ({ index, tags, instanceSlug }) => {
           <a href="stories/${story.id}/${story.id}.html">${story.metadata.title}${story.metadata.subtitle ? ' - ' + story.metadata.subtitle : ''}</a> - par ${story.metadata.authors.join()}
         </li>
         `
-  })
+  }).join('\n')
 }
   </ul>
 </div>
@@ -68,7 +68,7 @@ const buildTagArchive = ({ index, tags, tag }) => {
         <a href="stories/${story.id}/${story.id}.html">${story.metadata.title}${story.metadata.subtitle ? ' - ' + story.metadata.subtitle : ''}</a> - par ${story.metadata.authors.join()} (instance "${story.instanceSlug}")
       </li>
       `
-  })
+  }).join('\n')
 }
 </ul>
 `
@@ -93,7 +93,7 @@ const buildWholeArchive = ({ index, tags }) => {
           <a href="archive/instances/${instance.slug}/${story.id}/${story.id}.html">${story.metadata.title}${story.metadata.subtitle ? ' - ' + story.metadata.subtitle : ''}</a> - par ${story.metadata.authors.join()}
         </li>
         `
-  })
+  }).join('\n')
 }
   </ul>
 </div>
@@ -127,6 +127,65 @@ const buildArchiveIndex = ({
   return `
 <html>
   <head>
+    <!-- META DUBLIN CORE -->
+    <meta name="DC.Title" lang="fr" content="Quinoa archive" />
+    <meta name="DC.Date.created" scheme="W3CDTF" content="2017-09-01" />
+    <meta name="DC.subject" xml:lang="en-GB" content="rich content edition" />
+    <!-- END META DUBLIN CORE -->
+
+    <!-- REGULAR META -->
+    <meta name="author" content="médialab Sciences Po" />
+    <meta name="keywords" content="data stories, data, data comment, rich media, academic editing, scholarly media, scholarly editor, academic editor" />
+    <meta name="description" content="an archive of website made with fonio, a scholarly dissertations editor allowing to build standalone HTML webpages" />
+    <meta name="viewport" content="user-scalable=no,width=device-width" />
+    <!-- END REGULAR META -->
+
+    <!-- META TWITTER -->
+    <meta name="twitter:card" value="summary" />
+    <meta name="twitter:title" content="Quinoa archive" />
+    <meta name="twitter:description" content="an archive of website made with fonio, a scholarly dissertations editor allowing to build standalone HTML webpages" />
+    <!-- todo : Twitter Summary card images must be at least 200x200px -->
+    <!-- end meta twitter-->
+
+    <!-- META GOOGLE + -->
+    <meta itemprop="name" content="Quinoa archive" />
+    <meta itemprop="description" content="an archive of website made with fonio, a scholarly dissertations editor allowing to build standalone HTML webpages" />
+    <!-- END META GOOGLE + -->
+
+    <!-- META OPEN GRAPH / FACEBOOK -->
+    <meta property="og:title" content="Quinoa archive" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="an archive of website made with fonio, a scholarly dissertations editor allowing to build standalone HTML webpages" />
+    <!-- END META OPEN GRAPH / FACEBOOK -->
+    <style>
+        body{
+          display: flex;
+          flex-flow: column nowrap;
+          align-items: center;
+          font-family: sans-serif;
+        }
+        section,header{
+          max-width: 500px;
+        }
+        ul{
+          list-style: none;
+          padding: 0;
+          width: 100%;
+        }
+        li{
+          background: #333;
+          color: white;
+          margin-bottom: 1rem;
+          padding: 1rem;
+        }
+
+        li a{
+          color: white;
+          display: block;
+          font-size: 2rem;
+          margin-bottom: 1rem;
+        }
+      </style>
   </head>
   <body>
     <header>
